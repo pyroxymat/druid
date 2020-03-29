c5d8c24a4ec4752f9e5186c67b683812d6a4b61f
pyroxymat <pyroxymat@tutanota.com>
2020-03-29 13:02:55 +0200

    Implement simple timer example

--- druid/examples/timer.rs
+++ druid/examples/timer.rs
@@ -12,70 +12,257 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! An example of a timer.
+//! Simple countdown timer
 
+use druid::widget::{Button, Flex, Label, MainAxisAlignment, Painter};
+use druid::{
+    theme, AppLauncher, BoxConstraints, Color, Data, Env, Event, EventCtx, LayoutCtx, Lens,
+    LifeCycle, LifeCycleCtx, LocalizedString, PaintCtx, PlatformError, RenderContext, Selector,
+    Size, Target, TimerToken, UpdateCtx, Widget, WidgetExt, WidgetId, WindowDesc,
+};
 use std::time::{Duration, Instant};
 
-use druid::kurbo::Line;
-use druid::widget::prelude::*;
-use druid::{AppLauncher, Color, LocalizedString, TimerToken, WindowDesc};
+const TIMER_UPDATE_DELAY: Duration = Duration::from_millis(50);
 
-struct TimerWidget {
-    timer_id: TimerToken,
-    on: bool,
+const ROOT_WIDGET_ID: WidgetId = WidgetId::reserved(1);
+const CMD_START_TIMER: Selector = Selector::new("start_paint_timer");
+const CMD_STOP_TIMER: Selector = Selector::new("stop_paint_timer");
+
+#[derive(Clone, Debug, PartialEq)]
+enum TimerState {
+    Init,
+    Running {
+        started_at: Instant,
+        duration: Duration,
+    },
+    Stopped {
+        duration: Duration,
+    },
+    Completed,
 }
 
-impl Widget<u32> for TimerWidget {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, _data: &mut u32, _env: &Env) {
-        match event {
-            Event::MouseDown(_) => {
-                self.on = !self.on;
-                ctx.request_paint();
-                let deadline = Instant::now() + Duration::from_millis(500);
-                self.timer_id = ctx.request_timer(deadline);
-            }
-            Event::Timer(id) => {
-                if *id == self.timer_id {
-                    self.on = !self.on;
-                    ctx.request_paint();
-                    let deadline = Instant::now() + Duration::from_millis(500);
-                    self.timer_id = ctx.request_timer(deadline);
+#[derive(Clone, Lens, Data)]
+struct AppData {
+    text: String,
+    #[druid(same_fn = "PartialEq::eq")]
+    duration: Duration,
+    #[druid(same_fn = "PartialEq::eq")]
+    timer_state: TimerState,
+}
+
+impl AppData {
+    fn update(&mut self) {
+        self.text = match self.timer_state {
+            TimerState::Init => duration_as_human_readable(self.duration),
+            TimerState::Running {
+                started_at,
+                duration,
+            } => {
+                let duration_passed = Instant::now() - started_at;
+                let leftover_duration = duration.checked_sub(duration_passed);
+                if let Some(leftover_duration) = leftover_duration {
+                    duration_as_human_readable(leftover_duration)
+                } else {
+                    self.timer_state = TimerState::Completed;
+                    return self.update();
                 }
             }
-            _ => (),
+            TimerState::Stopped { duration } => duration_as_human_readable(duration),
+            TimerState::Completed => duration_as_human_readable(Duration::from_secs(0)),
+        };
+    }
+}
+
+struct RootWidget<T: Widget<AppData>> {
+    timer_id: TimerToken,
+    inner: T,
+}
+
+impl<T: Widget<AppData>> RootWidget<T> {
+    fn new(inner: T) -> Self {
+        Self {
+            timer_id: TimerToken::INVALID,
+            inner,
+        }
+    }
+}
+
+fn main() -> Result<(), PlatformError> {
+    let main_window = WindowDesc::new(ui_builder).title(
+        LocalizedString::new("styled-text-demo-window-title").with_placeholder("Type Styler"),
+    );
+
+    let default_duration = Duration::from_secs(5 * 60);
+    let data = AppData {
+        text: duration_as_human_readable(default_duration),
+        duration: default_duration,
+        timer_state: TimerState::Init,
+    };
+
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .launch(data)?;
+
+    Ok(())
+}
+
+impl<T: Widget<AppData>> Widget<AppData> for RootWidget<T> {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppData, env: &Env) {
+        let is_handled = match event {
+            Event::Timer(id) => {
+                if *id == self.timer_id {
+                    let deadline = Instant::now() + TIMER_UPDATE_DELAY;
+                    self.timer_id = ctx.request_timer(deadline);
+                    data.update();
+                    true
+                } else {
+                    false
+                }
+            }
+            Event::Command(cmd) => {
+                if cmd.selector == CMD_START_TIMER {
+                    let deadline = Instant::now() + TIMER_UPDATE_DELAY;
+                    self.timer_id = ctx.request_timer(deadline);
+                    data.update();
+                    true
+                } else if cmd.selector == CMD_STOP_TIMER {
+                    self.timer_id = TimerToken::INVALID;
+                    data.update();
+                    true
+                } else {
+                    false
+                }
+            }
+            _ => false,
+        };
+
+        if !is_handled {
+            self.inner.event(ctx, event, data, env);
         }
     }
 
-    fn lifecycle(&mut self, _ctx: &mut LifeCycleCtx, _event: &LifeCycle, _data: &u32, _env: &Env) {}
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &AppData, env: &Env) {
+        self.inner.lifecycle(ctx, event, data, env);
+    }
 
-    fn update(&mut self, _ctx: &mut UpdateCtx, _old_data: &u32, _data: &u32, _env: &Env) {}
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &AppData, data: &AppData, env: &Env) {
+        self.inner.update(ctx, old_data, data, env);
+    }
 
     fn layout(
         &mut self,
-        _layout_ctx: &mut LayoutCtx,
+        layout_ctx: &mut LayoutCtx,
         bc: &BoxConstraints,
-        _data: &u32,
-        _env: &Env,
+        data: &AppData,
+        env: &Env,
     ) -> Size {
-        bc.constrain((100.0, 100.0))
+        self.inner.layout(layout_ctx, bc, data, env)
     }
 
-    fn paint(&mut self, ctx: &mut PaintCtx, _data: &u32, _env: &Env) {
-        if self.on {
-            ctx.stroke(Line::new((10.0, 10.0), (10.0, 50.0)), &Color::WHITE, 1.0);
-        }
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &AppData, env: &Env) {
+        self.inner.paint(ctx, data, env);
+    }
+
+    fn id(&self) -> Option<WidgetId> {
+        Some(ROOT_WIDGET_ID)
     }
 }
 
-fn main() {
-    let window = WindowDesc::new(|| TimerWidget {
-        timer_id: TimerToken::INVALID,
-        on: false,
-    })
-    .title(LocalizedString::new("timer-demo-window-title").with_placeholder("Tick Tock"));
+fn ui_builder() -> impl Widget<AppData> {
+    let my_painter = Painter::new(|ctx, _, _| {
+        let bounds = ctx.size().to_rect();
+        if ctx.is_hot() {
+            ctx.fill(bounds, &Color::rgba8(0, 0, 0, 128));
+        }
 
-    AppLauncher::with_window(window)
-        .use_simple_logger()
-        .launch(0u32)
-        .expect("launch failed");
+        if ctx.is_active() {
+            ctx.stroke(bounds, &Color::WHITE, 2.0);
+        }
+    });
+
+    let styled_label = Label::new(|data: &AppData, _env: &_| data.text.clone())
+        .with_text_color(theme::PRIMARY_LIGHT)
+        .with_text_size(24.0)
+        .background(my_painter);
+
+    let start_button = Button::new("Start")
+        .on_click(|ctx: &mut EventCtx, data: &mut AppData, _| {
+            match data.timer_state {
+                TimerState::Init => {
+                    data.timer_state = TimerState::Running {
+                        started_at: Instant::now(),
+                        duration: data.duration,
+                    };
+                }
+                TimerState::Stopped { duration } => {
+                    data.timer_state = TimerState::Running {
+                        started_at: Instant::now(),
+                        duration,
+                    };
+                }
+                _ => (),
+            }
+
+            // Targetting ROOT_WIDGET_ID doesn't works
+            ctx.submit_command(CMD_START_TIMER, Target::Global);
+        })
+        .fix_height(30.0);
+
+    let stop_button = Button::new("Stop")
+        .on_click(|ctx: &mut EventCtx, data: &mut AppData, _| {
+            if let TimerState::Running {
+                started_at,
+                duration,
+            } = data.timer_state
+            {
+                let duration_passed = Instant::now() - started_at;
+                let leftover_duration = duration.checked_sub(duration_passed);
+                if let Some(leftover_duration) = leftover_duration {
+                    data.timer_state = TimerState::Stopped {
+                        duration: leftover_duration,
+                    };
+                } else {
+                    data.timer_state = TimerState::Completed;
+                }
+            }
+            // Targetting ROOT_WIDGET_ID doesn't work
+            ctx.submit_command(CMD_STOP_TIMER, Target::Global);
+        })
+        .fix_height(30.0);
+
+    let reset_button = Button::new("Reset")
+        .on_click(|ctx: &mut EventCtx, data: &mut AppData, _| {
+            data.timer_state = TimerState::Init;
+            // Targetting ROOT_WIDGET_ID doesn't work
+            ctx.submit_command(CMD_STOP_TIMER, Target::Global);
+        })
+        .fix_height(30.0);
+
+    let layout_child = Flex::column()
+        .main_axis_alignment(MainAxisAlignment::Center)
+        .with_child(styled_label)
+        .with_spacer(8.0)
+        .with_child(
+            Flex::row()
+                .with_child(start_button)
+                .with_spacer(5.0)
+                .with_child(stop_button)
+                .with_spacer(5.0)
+                .with_child(reset_button),
+        );
+
+    RootWidget::new(layout_child)
+}
+
+fn duration_as_human_readable(duration: Duration) -> String {
+    let secs_total = duration.as_secs();
+    let secs = secs_total % 60;
+
+    let mins_total = secs_total / 60;
+    let mins = mins_total % 60;
+
+    // For simplicity sake we do not go further into days/years etc.
+    let hours = mins_total / 60;
+
+    format!("{:02}:{:02}:{:02}", hours, mins, secs)
 }
